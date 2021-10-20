f3c29f2593d49a7a48664de01e1b37f8a79efe49
zhangjian <49990990@qq.com>
2021-10-20 17:08:06 +0800

    no message

--- src/app/opencv-test/opencv-test.component.ts
+++ src/app/opencv-test/opencv-test.component.ts
@@ -11,23 +11,28 @@ import { Database } from '../core/service/database.service';
 import { LazyService } from '../core/service/lazy.service';
 import { OpenCVService } from '../core/service/opencv.service';
 import { OcradService } from '../core/service/ocrad.service';
+import {
+  canvasToBlob,
+  imageToCanvas,
+  loadImage,
+} from '../shared/util/image.util';
 // import * as OCRAD from 'ocrad.js';
 
 // declare const cv: any;
-declare const OCRAD: any;
+// declare const OCRAD: any;
 @Component({
   selector: 'app-opencv-test',
   templateUrl: './opencv-test.component.html',
   styleUrls: ['./opencv-test.component.scss'],
 })
 export class OpencvTestComponent implements OnInit {
+  @ViewChild('container')
+  container: ElementRef;
   imageSrc: SafeResourceUrl;
   ready: boolean;
   status = 'OpenCV.js is loading...';
   id: number;
   blob: Blob;
-  @ViewChild('container')
-  container: ElementRef;
   constructor(
     private lazy: LazyService,
     private sanitizer: DomSanitizer,
@@ -40,12 +45,12 @@ export class OpencvTestComponent implements OnInit {
 
   async ngOnInit() {
     this.opencvService.init();
-    this.route.queryParams.subscribe((params) => {
-      this.id = +params.id;
-      this.reload();
-    });
-    await this.lazy.loadScript('/assets/js/ocrad.js');
-    console.log(OCRAD);
+    // this.route.queryParams.subscribe((params) => {
+    //   this.id = +params.id;
+    //   this.reload();
+    // });
+    // await this.lazy.loadScript('/assets/js/ocrad.js');
+    // console.log(OCRAD);
     // await this.lazy.loadScript('/assets/js/opencv.js');
     // cv.then(() => {
     //   this.zone.run(() => {
@@ -56,90 +61,50 @@ export class OpencvTestComponent implements OnInit {
     // });
   }
 
-  async reload() {
-    const data = await this.database.preuploadFile.get(this.id);
+  async fileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const files = input.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files.item(i);
+      if (file.size > 1024 * 1024) {
+        const url = URL.createObjectURL(file);
+        try {
+          const image = await loadImage(url);
+
+          let scale: number;
+          if (image.height >= image.width && image.width > 1080) {
+            scale = 1080 / image.width;
+          } else if (image.height < image.width && image.height > 1080) {
+            scale = 1080 / image.height;
+          }
+          const canvas = imageToCanvas(image, scale);
+          const container = this.container.nativeElement as HTMLDivElement;
+          container.appendChild(canvas);
+          const blob = await canvasToBlob(canvas);
+          await this.reload(blob);
+        } finally {
+          URL.revokeObjectURL(url);
+        }
+      } else {
+        await this.reload(file);
+      }
+    }
+    input.value = null;
+  }
+
+  async reload(blob: Blob) {
+    // const data = await this.database.preuploadFile.get(this.id);
     // console.log(data);
     // this.blob = data.blob;
-    const result = await this.opencvService.debug(data.blob);
+    const result = await this.opencvService.debug(blob);
+
     const container = this.container.nativeElement as HTMLDivElement;
     for (const img of result) {
       const canvas = await this.opencvService.toCanvas(img);
       container.appendChild(canvas);
     }
-    // console.log(src);
-    // if(src.rows>src.cols){
-    //   this.opencvService.rotate(src,90);
-    // }
-    // const ratio = 900 / src.rows;
-    // const resize = this.opencvService.resizeImg(src, ratio);
-
-    // // cv.cvtColor(resize, resize, cv.COLOR_RGBA2RGB, 0);
-    // // cv.bilateralFilter(resize, dst2, 9, 75, 75, cv.BORDER_DEFAULT);
-    // // cv.medianBlur(resize, dst2, 5);
-    // // cv.imshow('canvasOutput0', dst2);
-    // const canny = this.opencvService.getCanny(resize);
-    // this.showCanny(canny);
-    // const maxContour = this.opencvService.findMaxContour(canny);
-    // canny.delete();
-    // this.showMaxContour(resize, maxContour);
-    // // let dst4 = cv.Mat.zeros(src.cols, src.rows, cv.CV_8UC3);
-    // let points = this.opencvService.getBoxPoint(maxContour, ratio);
-    // maxContour.delete();
-    // if (points.length !== 4) {
-    //   points = [
-    //     new cv.Point(0, 0),
-    //     new cv.Point(src.cols, 0),
-    //     new cv.Point(src.cols, src.rows),
-    //     new cv.Point(0, src.rows),
-    //   ];
-    // }
-    // this.showPoints(src, points);
-    // // points = this.orderPoints(points);
-    // // console.log(points);
-    // const dst = this.opencvService.warpImage(src, points);
-    // // const minColor = cv.matFromArray(1, 3, cv.CV_32S, [200, 200, 100]);
-    // // const maxColor = cv.matFromArray(1, 3, cv.CV_32S, [250, 250, 150]);
-    // // let low = new cv.Mat(src.rows, src.cols, src.type(), [150, 0, 0, 0] as any);
-    // // let high = new cv.Mat(src.rows, src.cols, src.type(), [
-    // //   255, 150, 150, 255,
-    // // ] as any);
-    // // console.log(cv);
-    // // const dst3 = new cv.Mat();
-    // // cv.inRange(src, low, high, dst3);
-    // // cv.imshow('canvasOutput0', dst3);
-    // this.showWarp(dst);
-    // // this.opencvService.resizeTo(dst, { maxHeight: 900 });
-    // const dst2 = this.opencvService.extractColor(resize);
-    // cv.imshow('canvasOutput5', dst2);
-    // const rect = this.opencvService.getCenterRect(dst2);
-    // if (rect) {
-    //   const rectangleColor = new cv.Scalar(255, 0, 0);
-    //   const dst3 = cv.Mat.zeros(resize.rows, resize.cols, cv.CV_8UC3);
-    //   const point1 = new cv.Point(rect.x, rect.y);
-    //   const point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
-    //   cv.rectangle(dst3, point1, point2, rectangleColor, -1);
-    //   cv.imshow('canvasOutput7', dst3);
-    //   const dst4 = this.opencvService.crop(dst2, rect);
-    //   console.log(dst4.rows);
-    //   this.opencvService.resizeTo(dst4, { minHeight: 50 });
-    //   cv.imshow('canvasOutput8', dst4);
-    //   const canvas8 = document.getElementById(
-    //     'canvasOutput8'
-    //   ) as HTMLCanvasElement;
-    //   const text = await this.ocradService.execute(canvas8, {
-    //     numeric: true,
-    //   });
-    //   console.log(text);
-    // }
-    // // You can try more different parameters
-
-    // resize.delete();
-    // // points.delete();
-    // src.delete();
-    // dst.delete();
-    // await this.opencvService.init();
-    // const src = cv.imread(data.blob);
-    // console.log(src)
+    // const canvas = await this.opencvService.toCanvas(result.dest);
+    // container.appendChild(canvas);
   }
 
   // async inputChange(e) {
